2b1f1844281d18d362847b082f2d0098aa283da4
Paul Saade <paulsaade@Wednesday.local>
2017-01-28 18:39:09 -0500

    Initial Commit

---
+++ Note It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Note It
+//
+//  Created by Paul Saade on 2017-01-28.
+//  Copyright © 2017 Paul Saade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
